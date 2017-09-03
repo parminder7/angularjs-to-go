e6f0b2698d0e42e8a34c3319db23b4e9305134c4
Aakash Singh <Aakash_Singh@ultimatesoftware.com>
2017-09-03 17:41:51 -0400

    Home page navbar with basic routing infra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'feedback', component: FeedbackComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,  { enableTracing: true }),
     BrowserModule
   ],
   providers: [],
