a3d6513e5593cceb5c7b25c8d7238e9b23a0c684
Ravjot <Ravjot@10.255.254.56>
2017-09-03 21:46:16 -0400

    changing the upper limit for password to 100 instead of 500

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
   constructor(private fb: FormBuilder) {
       this.rForm = fb.group({
       'username' : [null, Validators.required],
-      'password' : [null, Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(500)])],
+      'password' : [null, Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(100)])],
       'validate' : ''
     });
   }
