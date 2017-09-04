ca78b3d0610fa4696eee967ad968beb7c5b87418
Ravjot <Ravjot@10.255.254.56>
2017-09-03 21:41:08 -0400

    applying the review comments

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,36 +10,22 @@ export class LoginComponent implements OnInit {
   rForm: FormGroup;
   post: any;                     // A property for our submitted form
   password: string = '';
-  name: string = '';
-  titleAlert: string = 'This field is required';
+  username: string = '';
+  fieldRequiredAlert: string = 'This field is required';
   constructor(private fb: FormBuilder) {
       this.rForm = fb.group({
-      'name' : [null, Validators.required],
-//      'password' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
-      'password' : [null, Validators.required],
+      'username' : [null, Validators.required],
+      'password' : [null, Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(500)])],
       'validate' : ''
     });
   }
 
   ngOnInit() {
-    this.rForm.get('validate').valueChanges.subscribe(
-
-      (validate) => {
-
-          if (validate === '1') {
-              this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
-              this.titleAlert = 'You need to specify at least 3 characters';
-          } else {
-              this.rForm.get('name').setValidators(Validators.required);
-          }
-          this.rForm.get('name').updateValueAndValidity();
-
-      });
   }
 
   addPost(post) { // here call the backend service
     this.password = post.password;
-    this.name = post.name;
+    this.username = post.username;
   }
 }
 
