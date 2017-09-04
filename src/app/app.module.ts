369e2c124e872efff3be0ce274b1f66e70547ce7
Ravjot Singh <ravjotsingh9@yahoo.com>
2017-09-03 21:48:39 -0400

    login module front end (#6)
    * login module front end
    
    * updating
    
    * removing dupilicate FormsComponents
    
    * changed the location js cnd
    
    * removing extra spaces
    
    * removed extra spaces
    
    * applying the review comments
    
    * changing the upper limit for password to 100 instead of 500

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: 'feedback', component: FeedbackComponent }
+  { path: 'feedback', component: FeedbackComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeedbackComponent
+    FeedbackComponent,
+    LoginComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes,  { enableTracing: true }),
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  rForm: FormGroup;
+  post: any;                     // A property for our submitted form
+  password: string = '';
+  username: string = '';
+  fieldRequiredAlert: string = 'This field is required';
+  constructor(private fb: FormBuilder) {
+      this.rForm = fb.group({
+      'username' : [null, Validators.required],
+      'password' : [null, Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(100)])],
+      'validate' : ''
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  addPost(post) { // here call the backend service
+    this.password = post.password;
+    this.username = post.username;
+  }
+}
+
